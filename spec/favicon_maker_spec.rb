f14f28225ff7649cc79047cc61892b33b28b8cfb
Jack Chu <kamuigt@gmail.com>
2012-11-14 14:37:59 -0500

    add iPad retina 144x144 support

--- spec/favicon_maker_spec.rb
+++ spec/favicon_maker_spec.rb
@@ -1,10 +1,10 @@
 require 'spec_helper'
 describe FaviconMaker, '#create_versions' do
-  
+
   before(:all) do
     @versions = []
     options = {
-      :versions => [:apple_114, :apple_57, :apple, :fav_png, :fav_ico],
+      :versions => [:apple_144, :apple_114, :apple_57, :apple, :fav_png, :fav_ico],
       :custom_versions => {:apple_extreme_retina => {:filename => "apple-touch-icon-228x228-precomposed.png", :dimensions => "228x228", :format => "png"}},
       :root_dir => File.join(Dir.pwd, "spec"),
       :input_dir => "support",
@@ -12,25 +12,25 @@ describe FaviconMaker, '#create_versions' do
       :output_dir => "generated",
       :copy => true
     }
-    
+
     @generated_dir = File.join(options[:root_dir], options[:output_dir])
     Dir.mkdir(@generated_dir)
-    
+
     FaviconMaker::Generator.create_versions(options) do |filepath|
       @versions << filepath
     end
   end
-  
-  it "creates 6 different versions" do
-    @versions.size.should eql(6)
+
+  it "creates 7 different versions" do
+    @versions.size.should eql(7)
   end
 
-  it "creates 6 files" do
+  it "creates 7 files" do
     @versions.each do |file|
       File.exists?(file).should be_true
     end
   end
-  
+
   after(:all) do
     @versions.each do |file|
       File.delete(file)
