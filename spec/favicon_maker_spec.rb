f70ace33c1034efebafa392e5020ad0d66b604f4
Andreas Follmann <andreas@toyrocketscience.com>
2014-01-09 23:27:58 +0100

    structure generator internals logically

--- spec/favicon_maker_spec.rb
+++ spec/favicon_maker_spec.rb
@@ -1,12 +1,35 @@
 require 'spec_helper'
+
 describe FaviconMaker, '#create_versions' do
 
   before(:all) do
     @multi_versions = []
     @uni_versions = []
     @options = {
-      :versions => [:apple_152, :apple_144, :apple_120, :apple_114, :apple_76, :apple_72, :apple_60, :apple_57, :apple, :fav_196, :fav_160, :fav_96, :fav_32, :fav_16, :fav_png, :fav_ico, :mstile_144],
-      :custom_versions => {:apple_extreme_retina => {:filename => "apple-touch-icon-228x228-precomposed.png", :sizes => "228x228", :format => "png"}},
+      :versions => [
+        :apple_152,
+        :apple_144,
+        :apple_120,
+        :apple_114,
+        :apple_76,
+        :apple_72,
+        :apple_60,
+        :apple_57,
+        :apple,
+        :fav_196,
+        :fav_160,
+        :fav_96,
+        :fav_32,
+        :fav_16,
+        :fav_png,
+        :fav_ico,
+        :mstile_144
+      ],
+      :custom_versions => {
+        :apple_extreme_retina => {
+          :filename => "apple-touch-icon-228x228-precomposed.png", :sizes => "228x228", :format => "png"
+        }
+      },
       :root_dir => File.join(Dir.pwd, "spec"),
       :input_dir => "support",
       :copy => true
