43279e0419eef2d5b185815cdf09edde60629af1
Andreas Follmann <andreas@toyrocketscience.com>
2014-05-30 17:04:04 +0200

    improve naming and structure

---
+++ lib/favicon_maker/commands/png_command.rb
@@ -0,0 +1,21 @@
+module FaviconMaker
+  class PngCommand
+
+    include BaseCommand
+
+    def initialize(template_file_path, output_file_path, size, options)
+
+      convert_settings = [
+        [ :define,      "png:include-chunk=none,trns,gama"  ],
+        [ :format,      "png"                               ],
+        [ :resize,      size                                ],
+        [ :gravity,     "center"                            ],
+        [ :background,  "none"                              ],
+        [ :extent,      size                                ],
+      ]
+
+      compose(template_file_path, output_file_path, convert_settings, options, :png)
+
+    end
+  end
+end
